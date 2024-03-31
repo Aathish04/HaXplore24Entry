b30428e9760f12326d4d809151ea9d5031d12eb1
alameluk17 <alamukanna14@gmail.com>
2024-03-31 17:25:23 +0530

    fastapi update

--- backend/api.py
+++ backend/api.py
@@ -5,6 +5,9 @@ import backend.appwrite.appwrite_functionalities as appwrite_functionalities
 from backend.utils.digilocker import get_aadhaar,get_digilocker_flow_url
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from datetime import date
+import ocr
+from ipfs import ipfs
 app = FastAPI()
 templates = Jinja2Templates(directory="backend/static/templates")
 @app.get("/digilocker_request_result_webhook")
@@ -53,6 +56,55 @@ async def login(request:Request):
 async def init_digilocker_flow(request:Request):
     return get_digilocker_flow_url()
 
+
+@app.post("/evaluate_vitals")
+async def evaluate_vitals(request:Request):
+    s = await request.json()
+    video_binary = s["video"]
+    f = open("file.mp4","w")
+    f.write(video_binary)
+    f.close()
+
+@app.post("/perform_ocr")
+async def perform_ocr(request:Request):
+    #json format from frontend : {user_id:userid,record_type:prescription/report,type:pdf/jpg,binary:}
+    s = await request.json()
+    print(s)
+    return {"s":s}
+    date = str(date.today())
+    userid = s["user_id"]
+    record_type = s["record_type"]
+    type = s["type"]
+    binary = s["binary"]
+    if type == "pdf":
+        path = "/tmp/" + userid+"_"+record_type+"_"+date+"/.pdf"
+    else:
+        path = "/tmp/" + userid+"_"+record_type+"_"+date+"/.jpg"
+
+    f = open(path,"w")
+    f.write(binary)
+    f.close()
+    text_in_file = ocr.performOCR(path)
+    f = open(path,"w")
+    f.write(text_in_file)
+    f.close()
+    cid = ipfs.putFile(path)
+    # add to user document in appwrite
+    d = appwrite_functionalities.initialize()
+    databases = d["Databases"]
+    existing_document = databases.get_document(database_id = '66071664001e1106b8bc',
+                                               collection_id = '66079e7c000cd257b449',
+                                               document_id = userid)
+
+    result = databases.update_document(
+        database_id = '66071664001e1106b8bc',
+        collection_id = '66079e7c000cd257b449',
+        document_id = userid,
+        data = {}, # optional
+    )
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
